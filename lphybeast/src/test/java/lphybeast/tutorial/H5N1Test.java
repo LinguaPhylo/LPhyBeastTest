fe33b7fa7453d70784618709833a29b1721c7be4
walterxie <walter@cs.auckland.ac.nz>
2022-07-27 12:55:09 +1200

    add test for estimating transitions

--- lphybeast/src/test/java/lphybeast/tutorial/H5N1Test.java
+++ lphybeast/src/test/java/lphybeast/tutorial/H5N1Test.java
@@ -4,12 +4,14 @@ import beast.util.LogAnalyser;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author Walter Xie
@@ -54,21 +56,75 @@ public class H5N1Test {
         File file = new File(TestUtils.LOG_PATH + "h5n1.stc.out");
         assertTrue(file.exists(), "h5n1.stc.out");
 
-//        BufferedReader reader;
-//        try {
-//            reader = new BufferedReader(new FileReader(file));
-//            String line = reader.readLine();
-//            while (line != null) {
-//                System.out.println(line);
-//                // read next line
-//                line = reader.readLine();
-//            }
-//            reader.close();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+        boolean startToCount = false;
+        // Fujian=>Hunan  Guangdong=>Hunan  Guangxi=>Hunan  HongKong=>Hunan
+        String[] direction = new String[4];
+        int[] zero = new int[4];
+//        int[] one = new int[4];
+        int[] two = new int[4];
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String line = reader.readLine();
+            int locId = 0;
+            while (line != null) {
+                if (!startToCount)
+                    startToCount = line.trim().startsWith("Transition\t0\t1\t2");
+                if (startToCount) {
+                    // skip header
+                    line = reader.readLine();
+                    // then, line looks like : Fujian=>Fujian	39	15	450 ...
+                    if ( line.trim().contains("=>Hunan") && !line.trim().equals("Hunan=>Hunan") ) {
+                        String[] elem = line.split("\t");
+                        System.out.println(Arrays.toString(elem));
+                        // at least 4 columns : Transition	0	1	2
+                        assertTrue(elem.length > 3, "Transition array of " + elem[0]);
 
+                        assertTrue(locId < 4, "locId = " + locId);
 
+                        direction[locId] = elem[0];
+                        zero[locId] = Integer.parseInt(elem[1]);
+//                        one[locId] = Integer.parseInt(elem[2]);
+                        two[locId] = Integer.parseInt(elem[3]);
 
+                        locId += 1;
+                    }
+                    // until empty line
+                    if (line.trim().isEmpty()) break;
+                }
+                // read next line
+                line = reader.readLine();
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        final String TO = "Guangxi=>Hunan";
+        int maxId = getMaxId(zero);
+        assertTrue(maxId >= 0 && maxId < 4, "0 transition : maxId = " + maxId);
+        // not Guangxi=>Hunan
+        assertNotEquals(TO, direction[maxId], "0 transition");
+
+//        maxId = getMaxId(one);
+//        assertTrue(maxId >= 0 && maxId < 4, "1 transition : maxId = " + maxId);
+//        // yes Guangxi=>Hunan
+//        assertEquals(TO, direction[maxId], "1 transition"); // uncertain as low ESS
+
+        maxId = getMaxId(two);
+        assertTrue(maxId >= 0 && maxId < 4, "2 transition : maxId = " + maxId);
+        // yes Guangxi=>Hunan
+        assertEquals(TO, direction[maxId], "2 transition");
+    }
+
+    private int getMaxId(int[] ints) {
+        int max = 0;
+        int maxId = -1;
+        for (int i = 0; i < ints.length; i++) {
+            if (ints[i] > max) {
+                maxId = i;
+                max = ints[i];
+            }
+        }
+        return maxId;
     }
 }
