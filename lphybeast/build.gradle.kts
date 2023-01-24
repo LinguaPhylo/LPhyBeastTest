e84d92e741a2a68d92e8e763029437e5166d803b
Walter <walterxie@users.noreply.github.com>
2023-01-25 11:39:03 +1300

    require beast 2.7 in repo folder? #5

--- lphybeast/build.gradle.kts
+++ lphybeast/build.gradle.kts
@@ -13,6 +13,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
     testImplementation("org.apache.commons:commons-lang3:3.12.0")
     testImplementation(fileTree( "$rootDir/beast2.7/lib"))
+    testImplementation(fileTree( "$rootDir/beast2.7/lib/packages"))
 }
 
 tasks.test {
